d354f20469f21c7ec26b54d497c1e541c66d6227
Jorge Marimon <jorgemarimon88@gmail.com>
2023-10-27 17:10:40 +0200

    Añado elemento trampa. Generación aleatoria.

--- Halloween_Jorge/Alimenta_al_vampiro/alimenta_al_vampiro.py
+++ Halloween_Jorge/Alimenta_al_vampiro/alimenta_al_vampiro.py
@@ -19,11 +19,13 @@ player_starting_lives = 5
 player_velocity = 10
 blood_starting_velocity = 10
 blood_aceleration = .5
+
 buffer_distance = 100
 
 score = 0
 player_lives = player_starting_lives
 blood_velocity = blood_starting_velocity
+trick_velocity = 30
 
 # Establecer colores
 red = (255, 0, 0)
@@ -35,32 +37,32 @@ black = (0, 0, 0)
 font = pygame.font.Font('faceYourFears.ttf', 32)
 
 # Establecer texto
-score_text = font.render("Puntuación: " + str(score), True, red, darkred)
+score_text = font.render("Puntuación: " + str(score), True, red, black)
 score_rect = score_text.get_rect()
 score_rect.topleft = (10, 10)  # 10 pixeles hacia abajo y 10 pixeles hacia dentro
 
-title_text = font.render("Alimenta al Vampiro", True, red, white)
+title_text = font.render("Alimenta al Vampiro", True, red, darkred)
 title_rect = title_text.get_rect()
 title_rect.centerx = window_width // 2
 title_rect.y = 10  # Así queda alineado con la puntuación, que era 10, 10 (recuerda, el primer 10 es 10
 # pixeles hacia abajo
 
-lives_text = font.render("Vidas: " + str(player_lives), True, red, darkred)
+lives_text = font.render("Vidas: " + str(player_lives), True, red, black)
 lives_rect = lives_text.get_rect()
 lives_rect.topright = (window_width - 10, 10)  # Esto primero coloca las vidas arriba a la derecha, luego le
 # digo que lo haga en el ancho de pantalla menos 10, para que no se quede pegada del tod a la derecha y el siguiente
 # 0 es para que está 0 pixeles hacia abajo del top
 
-game_over_text = font.render("HAS MUERTO", True, red, darkred)
+game_over_text = font.render("HAS MUERTO", True, red, black)
 game_over_rect = game_over_text.get_rect()
 game_over_rect.center = (window_width // 2, window_height // 2)
 
-continue_text = font.render("Pulsa cualquier tecla para CONTINUAR JUGANDO", True, red, darkred)
+continue_text = font.render("Pulsa cualquier tecla para CONTINUAR JUGANDO", True, red, black)
 continue_rect = continue_text.get_rect()
 continue_rect.center = (window_width // 2, window_height // 2 + 32)
 
-surprise_text = font.render("ENHORABUENA! Tu vampiro ya se ha saciado", True, red, darkred)
-surprise_text2 = font.render("y ahora te mira seductoramente", True, red, darkred)
+surprise_text = font.render("ENHORABUENA! Tu vampiro ya se ha saciado", True, red, black)
+surprise_text2 = font.render("y ahora te mira seductoramente", True, red, black)
 surprise_text_rect = surprise_text.get_rect()
 surprise_text_rect.center = (window_width // 2 - 170, window_height // 2)
 surprise_text2_rect = surprise_text.get_rect()
@@ -85,6 +87,8 @@ nico_game_over_sound = pygame.mixer.Sound("nico_derrota_sonido.wav")
 
 nico_victory_sound = pygame.mixer.Sound("nico_hetero.wav")
 
+error_trick_sound = pygame.mixer.Sound("error_pene_sound.wav")
+
 # Establecer imágenes
 player_image = pygame.image.load("vampiro.png")
 player_rect = player_image.get_rect()
@@ -92,9 +96,9 @@ player_rect.left = 10
 player_rect.centery = window_height // 2
 
 blood_image = pygame.image.load("sangre.png")
-blood_rect = blood_image.get_rect()
-blood_rect.x = window_width + buffer_distance
-blood_rect.y = random.randint(64, window_height - 32)  # Lo desplazamos 64 pixeles desde el top hacia abajo,
+blood_image_rect = blood_image.get_rect()
+blood_image_rect.x = window_width + buffer_distance
+blood_image_rect.y = random.randint(64, window_height - 32)  # Lo desplazamos 64 pixeles desde el top hacia abajo,
 # así nunca llegará más arriba, puesto que ahí tenemos la cabecera con los textos y luego le decimos que coja el alto
 # de la pantalla y que le reste 32, así si aleatoriamente la pusiese abajo del tod coincidiendo con la parte superior
 # de la imagen, no desaparecería, porque hay un límite de cuanto de abajo puede ponerla. 32 exactamente por que es
@@ -112,10 +116,27 @@ nico_lose_image = pygame.image.load("nico_lose.png")
 nico_lose_image_rect = nico_lose_image.get_rect()
 nico_lose_image_rect.center = (window_width // 2 + 400, window_height // 2)
 
+trick_image = pygame.image.load("pene_alado.png")
+trick_image_rect = trick_image.get_rect()
+trick_image_rect.x = window_width + buffer_distance
+trick_image_rect.y = random.randint(64, window_height - 32)
+
+# Variables adicionales para controlar los "tricks". Probablidad de que aparezca.
+trick_spawn_chance = 0.002
+
 # Bucle principal del juego
 pygame.mixer.music.play(-1, 0.0)  # El -1 indica un loop infinito y el 0.0 es que empieza desde el principio
 running = True
+
+# Variable para controlar si ya hay un trick en pantalla
+trick_on_screen = False
+
+# Variable para controlar si el "trick" debe reaparecer
+reappear_trick = False
+
 while running:
+    num_random = random.random()
+
     # Verificamos si el jugador quiere quitar el juego
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -129,38 +150,68 @@ while running:
     if keys[pygame.K_DOWN] and player_rect.bottom < window_height:
         player_rect.y += player_velocity
 
+    # Generar el "trick" ocasionalmente
+    if not trick_on_screen:
+        if num_random <= trick_spawn_chance:
+            if trick_image_rect.x > 0:
+                trick_image_rect.x = window_width + buffer_distance
+                trick_image_rect.y = random.randint(64, window_height - 32)
+                trick_on_screen = True
+    else:
+        if trick_image_rect.x < 0:
+            trick_on_screen = False
+        else:
+            trick_image_rect.x -= trick_velocity
+
     # Mover la sangre
-    if blood_rect.x < 0:  # Es decir, 0 es el lateral izquierdo, así que si es menor, es que el vampiro no ha atrapado
-        # la sangre, por lo que se la ha perdido
+    if blood_image_rect.x < 0:  # Es decir, 0 es el lateral izquierdo, así que si es menor, es que el vampiro no ha
+        # atrapado la sangre, por lo que se la ha perdido
         player_lives -= 1
         miss_sound.play()
-        blood_rect.x = window_width + buffer_distance  # Reposicionmos la sangre en el eje x, como al principio,
+        blood_image_rect.x = window_width + buffer_distance  # Reposicionmos la sangre en el eje x, como al principio,
         # sacándola de la pantalla un poco, para que no aparezca de la nada ya, sino que venga desde una zona donde
         # no la vemos
-        blood_rect.y = random.randint(64, window_height - 32)  # Para que, nuevamente, aparezca de manera
+        blood_image_rect.y = random.randint(64, window_height - 32)  # Para que, nuevamente, aparezca de manera
         # aleatoria en el eje y
     else:
         # Mover la sangre
-        blood_rect.x -= blood_velocity
+        blood_image_rect.x -= blood_velocity
+
+    # Mover el "trick"
+    if trick_on_screen:
+        if trick_image_rect.x < 0:
+            reappear_trick = True
+            trick_on_screen = False
 
     # Verificar las colisiones
-    if player_rect.colliderect(blood_rect):
+    if player_rect.colliderect(blood_image_rect):
         score += 1
         blood_sound.play()
         blood_velocity += blood_aceleration
-        blood_rect.x = window_width + buffer_distance  # Reposicionmos la sangre en el eje x, como al principio,
+        blood_image_rect.x = window_width + buffer_distance  # Reposicionmos la sangre en el eje x, como al principio,
         # sacándola de la pantalla un poco, para que no aparezca de la nada ya, sino que venga desde una zona donde
         # no la vemos
-        blood_rect.y = random.randint(64, window_height - 32)  # Para que, nuevamente, aparezca de manera
+        blood_image_rect.y = random.randint(64, window_height - 32)  # Para que, nuevamente, aparezca de manera
         # aleatoria en el eje y
+    # Verificar la colisión con el "trick"
+    elif trick_on_screen and player_rect.colliderect(trick_image_rect):
+        player_lives -= 1
+        error_trick_sound.play()
+        reappear_trick = True
+        trick_on_screen = False  # Si colisiona con el "trick", lo quitamos de la pantalla
+
+    # Generamos un nuevo trick si ya no hay ninguno en pantalla
+    if reappear_trick:
+        trick_image_rect.x = window_width + buffer_distance
+        trick_image_rect.y = random.randint(64, window_height - 32)
+        reappear_trick = False
 
     # Actualizamos el banner informativo superior
-    score_text = font.render("Puntuación: " + str(score), True, red, darkred)
-    lives_text = font.render("Vidas: " + str(player_lives), True, red, darkred)
+    score_text = font.render("Puntuación: " + str(score), True, red, black)
+    lives_text = font.render("Vidas: " + str(player_lives), True, red, black)
 
     # Verificar el game over
     if player_lives == 0:
-
         display_surface.blit(game_over_image, game_over_image_rect)
         display_surface.blit(game_over_text, game_over_rect)
         display_surface.blit(continue_text, continue_rect)
@@ -169,7 +220,6 @@ while running:
         pygame.display.update()
         pygame.time.delay(int(delay * 1000))
         nico_game_over_sound.play()
-
         # Parar el juego hasta que el jugador presione una tecla que hará empezar el juego nuevamente
         pygame.mixer.music.stop()
         is_paused = True
@@ -189,7 +239,7 @@ while running:
                     running = False
 
     # Insertamos la sorpresa
-    if score == 60:
+    if score == 57:
         display_surface.blit(nico_seductor_image, nico_seductor_image_rect)
         display_surface.blit(surprise_text, surprise_text_rect)
         display_surface.blit(surprise_text2, surprise_text2_rect)
@@ -219,7 +269,8 @@ while running:
 
     # Añadir nuestros recursos a la pantalla
     display_surface.blit(player_image, player_rect)
-    display_surface.blit(blood_image, blood_rect)
+    display_surface.blit(blood_image, blood_image_rect)
+    display_surface.blit(trick_image, trick_image_rect)
 
     # Actualizamos la pantalla y marcamos el reloj
     pygame.display.update()
